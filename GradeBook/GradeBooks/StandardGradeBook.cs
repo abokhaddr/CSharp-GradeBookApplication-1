bafd0b37c10aab9561a462071d86bf97d7d234dd
Oscar Corbalan <ocorbalan@icloud.com>
2019-05-04 18:21:04 +0200

    Creation

---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using GradeBook.Enums;
+
+namespace GradeBook.GradeBooks
+{
+    public class StandardGradeBook: BaseGradeBook
+    {
+        public StandardGradeBook(string name): base (name) // We're calling the constructore of the base class.
+        {
+            Type = GradeBookType.Standard;
+        }
+    }
+}
